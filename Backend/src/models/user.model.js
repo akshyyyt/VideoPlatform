5e8c6063ea909537c96819202a6a1a199bd805dd
akshyyyt <akshittomar5@gmail.com>
2026-06-10 02:10:53 +0530

    fix: Session schema bug fixes

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -79,7 +79,7 @@ userSchema.methods.generateRefreshToken = function (){
         {
             _id: this.id,
         },
-        process.env.REFRESN_TOKEN_SECRET,
+        process.env.REFRESH_TOKEN_SECRET,
         {
             expiresIn: process.env.REFRESH_TOKEN_EXPIRY
         } 
